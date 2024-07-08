ee346d61e514090417cd4a70ffdb5c49e2754d1b
Yosua Chrial Martono <yosua.chrial@ui.ac.id>
2024-07-08 15:29:58 +0700

    Chore: Added prisma to database, added theme provider, added sidenav placeholder

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 
+import { ThemeProvider } from "@/components/theme-provider";
+import SideNav from "@/components/side-nav/side-nav";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -16,7 +19,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={`${inter.className} h-screen`}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
+          <main className="flex gap-3 h-full">
+            <SideNav />
+            {children}
+          </main>
+        </ThemeProvider>
+      </body>
     </html>
   );
 }
---
+++ src/components/side-nav/side-nav.tsx
@@ -0,0 +1,7 @@
+export default function SideNav() {
+  return (
+    <div className="h-full p-4">
+      <div className="w-[280px] h-full bg-slate-200 rounded-md"></div>
+    </div>
+  );
+}
